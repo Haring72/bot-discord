06560d1e132fd41558aeb1694b30c7797ff169b4
Haring72 <megalan7210@gmail.com>
2026-01-20 04:45:55 +0100

    Cogs support for running commands with prefix

--- bot.py
+++ bot.py
@@ -1,15 +1,36 @@
+import os
 import discord
 import json
 from discord.ext import commands
 
+
+
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+intents.message_content = True
+
+# Check if necessary to use discord.Client or commands.Bot (first one commented for now)
+# client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix = '!', intents=intents)
+
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 with open('config.json') as f:
     config = json.load(f)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Started as {client.user}')
+    print(f'Started as {bot.user}')
 
-client.run(config['token'])
+
+
+async def main():
+    await load_cogs()
+    await bot.run(config['token'])
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
