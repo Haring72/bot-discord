e8a40ae969d1b7e8825c3b039ffc9e96749fd264
Haring72 <megalan7210@gmail.com>
2026-01-20 14:47:04 +0100

    Main code with cogs support

--- bot.py
+++ bot.py
@@ -1,15 +1,33 @@
+import os
 import discord
 import json
 from discord.ext import commands
 
+
+
 intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 with open('config.json') as f:
     config = json.load(f)
 
-@client.event
-async def on_ready():
-    print(f'Started as {client.user}')
+@bot.event
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
-client.run(config['token'])
+@bot.event
+
+async def on_ready():
+    print(f'Bot started as {bot.user}')
+
+import asyncio
+async def main():
+    await load_cogs()
+    print(f'Cogs loaded successfully.')
+    await bot.start(config['token'])
+
+if __name__ == '__main__':
+    asyncio.run(main())
