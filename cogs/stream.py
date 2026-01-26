6b19c39bee727e9331388acdcf6b551529788659
Haring72 <megalan7210@gmail.com>
2026-01-26 03:40:20 +0100

    Fixed syntax errors, definition problems, stream checks and spam issues. Also implemented credentials trough config.json

--- cogs/stream.py
+++ cogs/stream.py
@@ -1,31 +1,24 @@
 import discord
 import aiohttp
-import asyncio
+import json
 from discord.ext import commands, tasks
 from datetime import datetime
 
 class StreamCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.config = self.load_config()
         self.twitch_token = None
         self.channels = {} # {guild_id: {'channel_id': twitch_username}}
+        self.stream_states = {} # {guild_id: {channel_id: bool}}
+        self.check_twitch_streams.start()
     
-    @commands.command()
-    @commands.has_permissions(manage_guild=True)
-    async def trackTwitch(self, ctx, twitch_username: str, discord_channel: discord.TextChannel):
-        if discord_channel is None:
-            discord_channel = ctx.channel
-        if ctx.guild.id not in self.channels:
-            self.channels[ctx.guild.id] = {}
-        
-        self.channels[ctx.guild.id][discord_channel.id] = twitch_username.lower()
-
-        embed = discord.Embed(
-            title="Twitch Tracking",
-            description=f"Se notificarán los streams de **{twitch_username}** en {discord_channel.mention}",
-            color=discord.Color.red()
-        )
-        await ctx.send(embed = embed)
+    def load_config(self):
+        with open('config.json', 'r') as f:
+            return json.load(f)
+    
+    def cog_unload(self):
+        self.check_twitch_streams.cancel()
     
     @tasks.loop(minutes=5)
     async def check_twitch_streams(self):
@@ -35,38 +28,72 @@ class StreamCog(commands.Cog):
         async with aiohttp.ClientSession() as session:
             for guild_id, channels in self.channels.items():
                 guild = self.bot.get_guild(guild_id)
+
+                if not guild:
+                    continue
+
                 for channel_id, twitch_user in channels.items():
                     channel = guild.get_channel(channel_id)
                     if not channel:
                         continue
 
                     is_live = await self.check_stream_live(session, twitch_user)
-                    if is_live:
+                    state_key = (guild_id, channel_id)
+
+                    if is_live and not self.stream_states.get(state_key, False):
                         await self.send_live_notification(channel, is_live)
+                        self.stream_states[state_key] = True
+                    elif not is_live and self.stream_states.get(state_key, False):
+                        self.stream_states[state_key] = False
+    
+    @commands.command()
+    @commands.has_permissions(manage_guild = True)
+    async def trackTwitch(self, ctx, twitch_username: str, discord_channel: discord.TextChannel = None):
+        if discord_channel is None:
+            discord_channel = ctx.channel
+        if ctx.guild.id not in self.channels:
+            self.channels[ctx.guild.id] = {}
+        
+        self.channels[ctx.guild.id][discord_channel.id] = twitch_username.lower()
+        self.stream_states[(ctx.guild.id, discord_channel.id)] = False
+
+        embed = discord.Embed(
+            title="Twitch Tracking",
+            description=f"Se notificarán los streams de **{twitch_username}** en {discord_channel.mention}",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed = embed)
                     
     async def get_twitch_token(self):
         async with aiohttp.ClientSession() as session:
             async with session.post('https://id.twitch.tv/oauth2/token', data = {
-                'client_id': '',
-                'client_secret': '',
+                'client_id': self.config['twitch_app_credentials']['client_id'],
+                'client_secret': self.config['twitch_app_credentials']['client_secret'],
                 'grant_type': 'client_credentials'
             }) as resp:
                 data = await resp.json()
                 self.twitch_token = f"Bearer {data['access_token']}"
+                print("Twitch token obtained.")
+                print(f"Token expires in {data['expires_in']} seconds.")
     
     async def check_stream_live(self, session, username):
-        if not self.twitch_token:
+        if not self.twitch_token or not self.config:
             return None
-        
-        async with session.get('https://api.twitch.tv/helix/users', headers = {'Client-ID': '', 'Authorization': self.twitch_token}, params = {'login': username}) as resp:
+    
+        headers = {
+            'Client-ID': self.config['twitch_app_credentials']['client_id'],
+            'Authorization': self.twitch_token
+        }
+
+        async with session.get('https://api.twitch.tv/helix/users', headers = headers, params = {'login': username}) as resp:
             user_data = await resp.json()
             if not user_data['data']:
                 return None
             user_id = user_data['data'][0]['id']
 
-        async with session.get('https://api.twitch.tv/helix/streams', headers = {'Client-ID': '', 'Authorization': self.twitch_token}, params = {'user_id': user_id}) as resp:
+        async with session.get('https://api.twitch.tv/helix/streams', headers = headers, params = {'user_id': user_id}) as resp:
             stream_data = await resp.json()
-            if stream_data['data']:
+            if stream_data.get('data'):
                 stream = stream_data['data'][0]
                 return {
                     'username': username,
@@ -78,18 +105,18 @@ class StreamCog(commands.Cog):
                 }
             return None
         
-        async def send_live_notification(self, channel, stream_data):
-            embed = discord.Embed(
-                title = f"{stream_data['username'].title()} está ahora mismo en directo!",
-                description = f"**{stream_data['title']}**",
-                color = discord.Color.red(),
-                url = stream_data['url']
-            )
-            embed.add_field(name = "Jugando a ", value = stream_data['game'] or "Desconocido", inline = True)
-            embed.add_field(name = "Espectadores" , value = f"{stream_data['viewer_count']:,}", inline = True)
-            embed.set_image(url = stream_data['thumbnail_url'])
+    async def send_live_notification(self, channel, stream_data):
+        embed = discord.Embed(
+            title = f"{stream_data['username'].title()} está ahora mismo en directo!",
+            description = f"**{stream_data['title']}**",
+            color = discord.Color.red(),
+            url = stream_data['url']
+        )
+        embed.add_field(name = "Jugando a ", value = stream_data['game'] or "Desconocido", inline = True)
+        embed.add_field(name = "Espectadores" , value = f"{stream_data['viewer_count']:,}", inline = True)
+        embed.set_image(url = stream_data['thumbnail_url'])
 
-            await channel.send(embed = embed)
+        await channel.send(embed = embed)
 
 async def setup(bot):
     await bot.add_cog(StreamCog(bot))
